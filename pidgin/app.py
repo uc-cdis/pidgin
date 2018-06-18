899c66f76291efce2da03c682215d34719f1c510
Pauline Ribeyre <ribeyre@uchicago.edu>
2018-06-18 16:40:48 -0500

    fix(query): fixed bug in flatten_dict() when the query output is empty

--- pidgin/app.py
+++ pidgin/app.py
@@ -3,7 +3,9 @@ import json
 import flask
 import requests
 
+
 app = flask.Flask(__name__)
+
 
 #JSON GET endpoint
 #@app.route('/json', methods=['GET'])
@@ -11,16 +13,19 @@ app = flask.Flask(__name__)
 #    data = ...
 #    return data
 
+
 # Endpoint to get core metadata from an object_id.
 @app.route('/json/<path:object_id>') # 'path' allows the use of '/' in the id
 def get_json_metadata(object_id):
     metadata = get_metadata_dict(object_id)
     return translate_dict_to_json(metadata)
 
+
 # Translate a dictionary to a JSON string.
 def translate_dict_to_json(d):
     json_str = json.dumps(d)
     return json_str
+
 
 # Create a dictionary containing the metadata for a given object_id.
 def get_metadata_dict(object_id):
@@ -35,42 +40,37 @@ def get_metadata_dict(object_id):
     #    print(key, value)
     return metadata
 
-# Flatten a dictionary, assuming there are no key duplicates.
+
+# Flatten a dictionary, assuming there are no duplicates in the keys.
 def flatten_dict(d):
     flat_d = {}
     for k, v in d.items():
         if isinstance(v, list):
-            flat_d.update(flatten_dict(v[0]))
+            if (v): # check if there is data to read
+                # object_id is unique so the list should only contain one item
+                flat_d.update(flatten_dict(v[0]))
         else:
             flat_d.update({k:v})
     return flat_d
 
+
 # Write a query and transmit it to send_query().
 def query_metadata(object_id):
-    file_type = 'submitted_aligned_reads'
+    object_id = "f4a34430-9f3d-4f0b-b0eb-c50b0df87062"
+    file_type = 'submitted_aligned_reads' # TODO: get file type from object_id
+    #query_txt = '{ ' + file_type + """ {
     query_txt = '{ ' + file_type + ' (object_id: "' + object_id + """") {
         core_metadata_collections {
-          title
-          description
-          creator
-          contributor
-          coverage
-          language
-          publisher
-          rights
-          source
-          subject
+          title description creator contributor coverage
+          language publisher rights source subject
         }
-        file_name
-        data_format
-        file_size
-        object_id
-        updated_datetime
+        file_name data_format file_size object_id updated_datetime
       }
     }"""
     #print(query_txt)
     data = send_query(query_txt)
     return data
+
 
 # Send a query to peregrine and return the jsonified response.
 def send_query(query_txt):
@@ -85,26 +85,6 @@ def send_query(query_txt):
 
     output = requests.post(api_url, headers={'Authorization': auth}, json=query).text
     #print(output)
-
-    # TODO: remove hardcoded output
-    output = """{
-      "data": {
-        "aligned_reads_index": [
-          {
-            "core_metadata_collections": [
-              {
-                "contributor": "DCP",
-                "coverage": "US",
-                "creator": "Pauline",
-                "language": "en-US"
-              }
-            ],
-            "data_format": "JPG",
-            "file_name": "pauline_test.jpg"
-          }
-        ]
-      }
-    }"""
 
     data = json.loads(output)
     return flask.jsonify(data)
