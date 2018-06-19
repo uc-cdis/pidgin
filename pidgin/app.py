574da094bc78b0d20f9f71404cea8eebdcc138df
Pauline Ribeyre <ribeyre@uchicago.edu>
2018-06-19 14:53:07 -0500

    feat(query): get file type from object_id by checking all file types. to fix

--- pidgin/app.py
+++ pidgin/app.py
@@ -7,18 +7,19 @@ import requests
 app = flask.Flask(__name__)
 
 
-#JSON GET endpoint
-#@app.route('/json', methods=['GET'])
-#def get-json-metadata():
-#    data = ...
-#    return data
-
-
-# Endpoint to get core metadata from an object_id.
+# Endpoint to get core metadata as JSON from an object_id.
 @app.route('/json/<path:object_id>') # 'path' allows the use of '/' in the id
 def get_json_metadata(object_id):
     metadata = get_metadata_dict(object_id)
     return translate_dict_to_json(metadata)
+
+
+# Create a dictionary containing the metadata for a given object_id.
+def get_metadata_dict(object_id):
+    response = request_metadata(object_id) # query to peregrine
+    data = response.get_json()
+    metadata = flatten_dict(data) # translate the response to a dictionary
+    return metadata
 
 
 # Translate a dictionary to a JSON string.
@@ -27,53 +28,79 @@ def translate_dict_to_json(d):
     return json_str
 
 
-# Create a dictionary containing the metadata for a given object_id.
-def get_metadata_dict(object_id):
-    response = query_metadata(object_id) # query to peregrine
-
-    # translate the response to a dictionary
-    data = response.get_json()['data']
-    #file_type = next(iter(data.keys())) # name of first key = file type
-    #metadata = data[file_type][0] # get metadata
-    metadata = flatten_dict(data)
-    #for key, value in metadata.items():
-    #    print(key, value)
-    return metadata
-
-
 # Flatten a dictionary, assuming there are no duplicates in the keys.
 def flatten_dict(d):
     flat_d = {}
     for k, v in d.items():
         if isinstance(v, list):
-            if (v): # check if there is data to read
+            if v: # check if there is data to read
                 # object_id is unique so the list should only contain one item
                 flat_d.update(flatten_dict(v[0]))
+        elif isinstance(v, dict):
+            if v: # check if there is data to read
+                flat_d.update(flatten_dict(v))
         else:
             flat_d.update({k:v})
     return flat_d
 
 
+# Return a list of the values stored in a dict and its nested dict.
+def get_nested_dict_values(d):
+    values = []
+    for k, v in d.items():
+        if isinstance(v, list):
+            for e in v:
+                values.extend(get_nested_dict_values(e))
+        elif isinstance(v, dict):
+            values.extend(get_nested_dict_values(v))
+        else:
+            values.append(v)
+    return values
+
+
+# Get the type of file from the object_id.
+def get_file_type(object_id):
+    #TODO use this instead if object_id is added to node
+    #query_txt = '{ node(object_id: "' + object_id + ') { node { type } }'
+    #file_type = send_query(query_txt)
+    #print(file_type.get_json())
+
+    # get the list of available types from the graphql schema
+    query_txt = '{ __schema { types { name } } }'
+    all_file_types = send_query(query_txt)
+    all_file_types = get_nested_dict_values(all_file_types.get_json())
+
+    # for each type, check if it contains the file with this object_id
+    for t in all_file_types:
+        query_txt = '{ ' + t + ' (object_id: "' + object_id + '")' + ' { id } }'
+        response = send_query(query_txt)
+        response = response.get_json()
+        if response['data'] and response['data'][t]:
+            return t
+        return 'submitted_aligned_reads' #TODO remove
+
+    #TODO here error: this object_id does not exist
+
+
 # Write a query and transmit it to send_query().
-def query_metadata(object_id):
-    object_id = "f4a34430-9f3d-4f0b-b0eb-c50b0df87062"
-    file_type = 'submitted_aligned_reads' # TODO: get file type from object_id
-    #query_txt = '{ ' + file_type + """ {
+def request_metadata(object_id):
+    file_type = get_file_type(object_id)
+
+    # get the metadata from the type of file and the object_id
     query_txt = '{ ' + file_type + ' (object_id: "' + object_id + """") {
         core_metadata_collections {
-          title description creator contributor coverage
-          language publisher rights source subject
+            title description creator contributor coverage
+            language publisher rights source subject
         }
-        file_name data_format file_size object_id updated_datetime
-      }
+        file_name data_format file_size object_id updated_datetime }
     }"""
-    #print(query_txt)
     data = send_query(query_txt)
     return data
 
 
 # Send a query to peregrine and return the jsonified response.
 def send_query(query_txt):
+    print(query_txt)
     query = {'query': query_txt}
 
     api_url = app.config.get('API_URL')
