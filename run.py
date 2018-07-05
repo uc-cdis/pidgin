a81a2cec8f161ed83c38bf816d5fd85b10960991
Pauline Ribeyre <ribeyre@uchicago.edu>
2018-07-05 11:51:26 -0500

    fix(run.py)

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 from pidgin.app import app
 
-app.config['API_URL'] = 'https://pauline.planx-pla.net/api/v0/submission/graphql/'
+app.config['API_URL'] = 'https://localhost/v0/submission/graphql/'
 app.run('127.0.0.1', 5000, debug=True)
