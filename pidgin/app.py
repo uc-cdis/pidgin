c2aeeb0c57c7af4e1d43fe48da645ef991635a96
Pauline Ribeyre <ribeyre@uchicago.edu>
2018-07-19 14:53:20 -0500

    PXD-1162 Fix/memory issue (#10)
    * fix(coremetadata-endpoint)
    
    * fix(memory-issue): remove recursive and update Dockerfile
    
    * feat(coremetadata-endpoint): error handling
    
    * fix(memory)
    
    * feat(unit-tests)
    
    * feat(.travis)

--- pidgin/app.py
+++ pidgin/app.py
@@ -1,125 +1,121 @@
-import json
-
 import flask
+import json
 import requests
+
+from pidgin.errors import *
 
 
 app = flask.Flask(__name__)
 
 
-# Endpoint to get core metadata as JSON from an object_id.
 @app.route('/json/<path:object_id>') # 'path' allows the use of '/' in the id
 def get_json_metadata(object_id):
-    metadata = get_metadata_dict(object_id)
-    return translate_dict_to_json(metadata)
+    """
+    Get core metadata as JSON from an object_id.
+    """
+    try:
+        metadata = get_metadata_dict(object_id)
+        return json.dumps(metadata) # translate dictionary to json
+    except PidginException as e:
+        return e.message, e.code
 
 
-# Endpoint to get core metadata as BibTeX from an object_id.
-@app.route('/bibtex/<path:object_id>') # 'path' allows the use of '/' in the id
+@app.route('/bibtex/<path:object_id>')
 def get_bibtex_metadata(object_id):
+    """
+    Get core metadata as BibTeX from an object_id.
+    """
     metadata = get_metadata_dict(object_id)
     return translate_dict_to_bibtex(metadata)
 
 
-# Create a dictionary containing the metadata for a given object_id.
 def get_metadata_dict(object_id):
+    """
+    Create a dictionary containing the metadata for a given object_id.
+    """
     response = request_metadata(object_id) # query to peregrine
     data = response.get_json()
-    metadata = flatten_dict(data) # translate the response to a dictionary
-    return metadata
+    return flatten_dict(data) # translate the response to a dictionary
 
 
-# Translate a dictionary to a JSON string.
-def translate_dict_to_json(d):
-    json_str = json.dumps(d)
-    return json_str
-
-
-# Translate a dictionary to a BibTeX string.
 def translate_dict_to_bibtex(d):
-    bibtex_str = '@misc {' + d['object_id'] + ', '
-    for k, v in d.items(): # add each pair to the BibTeX output
-        bibtex_str += k + ' = "' + str(v) + '", '
-    bibtex_str += '}'
+    """
+    Translate a dictionary to a BibTeX string.
+    """
+    items = ['{} = "{}"'.format(k, v) for k, v in d.items()]
+    bibtex_items = ', '.join(items)
+    bibtex_str = '@misc {' + d['object_id'] + ', ' + bibtex_items + '}'
     return bibtex_str
 
 
-# Flatten a dictionary, assuming there are no duplicates in the keys.
 def flatten_dict(d):
+    """
+    Flatten a dictionary that contains core metadata.
+    """
     flat_d = {}
-    for k, v in d.items():
-        if isinstance(v, list):
-            if v: # check if there is data to read
+    try:
+        data_type = list(d['data'].keys())[0]
+        for k, v in d['data'][data_type][0].items():
+            if k == 'core_metadata_collections':
                 # object_id is unique so the list should only contain one item
-                flat_d.update(flatten_dict(v[0]))
-        elif isinstance(v, dict):
-            if v: # check if there is data to read
-                flat_d.update(flatten_dict(v))
-        else:
-            flat_d.update({k:v})
+                for k, v in v[0].items():
+                    flat_d[k] = v
+            else:
+                flat_d[k] = v
+    except (AttributeError, IndexError):
+        error = 'Core metadata not available for this file'
+        if 'errors' in d:
+            error += ': ' + d['errors'][0]
+        raise NoCoreMetadataException(error)
     return flat_d
 
 
-# Return a list of the values stored in a dict and its nested dict.
-def get_nested_dict_values(d):
-    values = []
-    for k, v in d.items():
-        if isinstance(v, list):
-            for e in v:
-                values.extend(get_nested_dict_values(e))
-        elif isinstance(v, dict):
-            values.extend(get_nested_dict_values(v))
-        else:
-            values.append(v)
-    return values
-
-
-# Get the type of file from the object_id.
 def get_file_type(object_id):
+    """
+    Get the type of file from the object_id.
+    """
     query_txt = '{ datanode (object_id: "' + object_id + '") { type } }'
     response = send_query(query_txt)
     try:
         file_type = response.get_json()['data']['datanode'][0]['type']
     except IndexError:
-        raise Exception("Error: object_id not found")
+        raise ObjectNotFoundException('object_id "' + object_id + '" not found')
     return file_type
 
 
-# Write a query and transmit it to send_query().
 def request_metadata(object_id):
+    """
+    Write a query and transmit it to send_query().
+    """
+    file_type = get_file_type(object_id)
 
     # get the metadata from the type of file and the object_id
-    file_type = get_file_type(object_id)
-    query_txt = '{ ' + file_type + ' (object_id: "' + object_id + """") {
-        core_metadata_collections {
+    query_txt = '''{{ {} (object_id: "{}") {{
+        core_metadata_collections {{
             title description creator contributor coverage
             language publisher rights source subject
-        }
-        file_name data_type data_format file_size object_id updated_datetime }
-    }"""
-    data = send_query(query_txt)
+        }}
+        type file_name data_format file_size
+        project_id object_id updated_datetime }} }}
+        '''.format(file_type, object_id)
+    return send_query(query_txt)
 
-    return data
 
-
-# Send a query to peregrine and return the jsonified response.
 def send_query(query_txt):
-    # print(query_txt)
-    query = {'query': query_txt}
-
+    """
+    Send a query to peregrine and return the jsonified response.
+    """
     api_url = app.config.get('API_URL')
     if not api_url:
-        raise Exception("Error: pidgin is not configured with API_URL")
-        # return flask.jsonify({'error': 'pidgin is not configured with API_URL'}), 500
+        raise PidginException('Pidgin is not configured with API_URL')
 
     auth = flask.request.headers.get('Authorization')
+    query = {'query': query_txt}
+    data = requests.post(api_url, headers={'Authorization': auth}, json=query).json()
 
-    output = requests.post(api_url, headers={'Authorization': auth}, json=query).text
-
-    data = json.loads(output)
     return flask.jsonify(data)
 
-# Health check endpoint.
+  
 @app.route('/_status', methods=['GET'])
 def health_check():
     return 'Healthy', 200
