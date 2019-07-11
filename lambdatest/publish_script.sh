98e73616fd2775b1818894b5e028fba7b68740dc
Pauline Ribeyre <ribeyre@uchicago.edu>
2019-07-11 10:46:43 -0500

    chore(black): add wool check

--- lambdatest/publish_script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-MYVAR="deploy"
-zip from_travis_$MYVAR.zip lambda_function.py
-
-# zip from_travis_$TRAVIS_BRANCH.zip lambda_function.py
-# aws lambda publish-version --function-name from_travis_$TRAVIS_BRANCH
